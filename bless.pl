dd9cc2719ecc7bb81d81316ccef32e3529f90f5f
Andrew Solomon <andrew@geekuni.com>
2017-11-11 18:51:46 +0000

    bless

---
+++ bless.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+{
+  package Person;
+
+  sub new {
+    my $class = shift;
+    my $obj = {};
+    bless( $obj, $class);
+    return $obj;
+  }
+
+  sub set_name {
+    my ($self, $name) = @_;
+
+    $self->{name} = $name;
+  }
+
+  sub get_name {
+    my $self = shift;
+
+    return $self->{name};
+  }
+
+}
+
+
+my $person = Person->new;
+$person->set_name("Freddy");
+say "Name is: ".$person->get_name;
